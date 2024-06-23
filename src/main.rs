fc130944c69b317638c7f8cc74ef4a4ed82ffea4
sravan <sravan-s@protonmail.com>
2024-06-23 22:00:37 +0900

    Convert to async component

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use gtk::prelude::{BoxExt, GtkWindowExt};
-use relm4::{gtk, ComponentParts, ComponentSender, RelmApp, RelmWidgetExt, SimpleComponent};
+use relm4::{
+    component::{AsyncComponentParts, SimpleAsyncComponent},
+    gtk, AsyncComponentSender, RelmApp, RelmWidgetExt,
+};
 use sysinfo::System;
 
 #[derive(Debug, Clone)]
@@ -36,7 +39,7 @@ fn static_label(sa: StaticAssets) -> String {
     )
 }
 
-impl SimpleComponent for AppModel {
+impl SimpleAsyncComponent for AppModel {
     /// The type of the messages that this component can receive.
     type Input = AppUpdate;
     /// The type of the messages that this component can send.
@@ -55,11 +58,11 @@ impl SimpleComponent for AppModel {
             .default_height(600)
             .build()
     }
-    fn init(
+    async fn init(
         initial_data: Self::Init,
         window: Self::Root,
-        sender: ComponentSender<Self>,
-    ) -> relm4::ComponentParts<Self> {
+        sender: AsyncComponentSender<Self>,
+    ) -> AsyncComponentParts<Self> {
         let model = AppModel {
             static_assets: initial_data.clone(),
         };
@@ -76,12 +79,12 @@ impl SimpleComponent for AppModel {
         vbox.append(&label);
         let widgets = AppWidgets { label };
 
-        ComponentParts { model, widgets }
+        AsyncComponentParts { model, widgets }
     }
 
-    fn update(&mut self, message: Self::Input, _sender: ComponentSender<Self>) {}
+    async fn update(&mut self, message: Self::Input, _sender: AsyncComponentSender<Self>) {}
 
-    fn update_view(&self, widgets: &mut Self::Widgets, _sender: ComponentSender<Self>) {
+    fn update_view(&self, widgets: &mut Self::Widgets, _sender: AsyncComponentSender<Self>) {
         widgets
             .label
             .set_label(&static_label(self.static_assets.clone()));
@@ -92,22 +95,11 @@ fn main() {
     let sys = System::new_all();
     // Display system information:
     let app = RelmApp::new("relm4.test.simple_manual");
-    app.run::<AppModel>(StaticAssets {
+    app.run_async::<AppModel>(StaticAssets {
         name: System::name().unwrap(),
         cpus: sys.cpus().len(),
         host_name: System::host_name().unwrap(),
         kernel_version: System::kernel_version().unwrap(),
         os_version: System::os_version().unwrap(),
     });
-    /*
-    loop {
-        sys.refresh_all();
-
-        // RAM and swap information:
-        let total_memory = format!("total memory: {} bytes", sys.total_memory());
-        let used_memory = format!("used memory : {} bytes", sys.used_memory());
-        let total_swap = format!("total swap  : {} bytes", sys.total_swap());
-        let used_swap = format!("used swap   : {} bytes", sys.used_swap());
-    }
-    */
 }
